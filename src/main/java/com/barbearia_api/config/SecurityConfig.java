9389ad5ffdb4bc564fe085beaaf29bfe046f7130
valentim <valentimdesenvolvedor@gmail.com>
2025-06-03 17:31:59 -0300

    final build

--- src/main/java/com/barbearia_api/config/SecurityConfig.java
+++ src/main/java/com/barbearia_api/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://pretofosco.vercel.app/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://main.d3i55ze1zdi5bm.amplifyapp.com/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
